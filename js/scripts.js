df61f67f7b5ebc7082b0ea6fd1a907a56033f5f3
Ashley Kolodziej <kolodz10@bu.edu>
2023-12-06 22:31:05 -0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-// This is where you should write all JavaScript
-// for your project. Remember a few things as you start!
-// - Use let or const for all variables
-// - Do not use jQuery - use JavaScript instead
-// - Do not use onclick - use addEventListener instead
-// - Run npm run test regularly to check autograding
-// - You'll need to link this file to your HTML :)
+// This file is eventually where you'll learn to write JavaScript.
+// For right now, you don't need to worry about it.
+// script.js
+console.log('Script loaded successfully');
+
+function redirectToHome() {
+    window.location.href = './start_screen_1.html';
+  }
+  
+  // Add any additional JavaScript code here
+  document.addEventListener('DOMContentLoaded', function() {
+    var imageWrapper = document.getElementById('imageWrapper');
+  
+    // Add hover effect
+    imageWrapper.addEventListener('mouseover', function() {
+      document.querySelector('.image').style.opacity = '0.8';
+    });
+  
+    imageWrapper.addEventListener('mouseout', function() {
+      document.querySelector('.image').style.opacity = '1';
+    });
+  });
+  
